33e8dc629f7b12dc753184df17c7e764a62b0f07
Albert Tulo <atulo@blacklineit.com>
2026-04-09 12:15:20 -0500

    On branch feature/pos-system  Changes to be committed: 	new file:   pos/main.py 	new file:   pos/menu_item.py 	new file:   pos/order.py added main.py, menu_item.py and order.py. Created the class for menu_item and then using menu_item in order.py to have subtotal, tax, remove and add items, and list each line item out with the subottal. tax and total

---
+++ pos/order.py
@@ -0,0 +1,42 @@
+from menu_item import MenuItem
+
+class Order:
+    def __init__(self, customer_name: str):
+        self.customer_name = customer_name
+        self.items: list[MenuItem] = []
+    
+    def add_item(self, item: MenuItem) -> None:
+        self.items.append(item)
+
+    def remove_item(self, index: int) -> None:
+        actual_index = index - 1
+
+        if 0 <= actual_index < len(self.items):
+            removed_item = self.item.pop(actual_index)
+            print(f"Removed {removed_item}")
+        else:
+            print("Invalid item number. Nothing removed.")
+        
+        def subtotal(self) -> float:
+            return sum(item.price for item in self.items)
+        
+        def tax(self) -> float:
+            return self.subtotal() * 0.0875 ## 8.75%
+        
+        def __str__(self) -> str:
+            lineItems = []
+            lineItems.append(f"Customer: {self.customer_name}")
+            lineItems.append("-" * 30)
+
+            if not self.items:
+                lineItems.append("No items in order.")
+            else:
+                for i, item in enumerate(self.items, start=1):
+                    lineItems.append(f"{i}. {item}")
+            
+            lineItems.append("-" * 30)
+            lineItems.append(f"Subtotal: ${self.subtotal():.2f}")
+            lineItems.append(f"Tax (8.75%): ${self.tax():.2f}")
+            lineItems.append(f"Total: ${self.total():.2f}")
+
+            return "\n".join(lineItems)
